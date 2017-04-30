5befca6558de7e134ff334badb1bb6f86bd68d3e
Vlad <Vlad@INSANE>
2017-04-30 17:39:18 +0300

    population_chart

---
+++ population_chart/population_chart/Country.java
@@ -0,0 +1,35 @@
+package population_chart;
+
+public class Country 
+{
+	public String name;
+	public int population;
+	
+	public Country(String name, int population) 
+	{
+		super();
+		this.name = name;
+		this.population = population;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+	@Override
+	public String toString() {
+		return "Name: " + name + " population: " + population;
+	}
+}
